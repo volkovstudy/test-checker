5ac9ccfd564428a9cf0bea954f7deac83d94105c
Volkov Daniil <volkov.study@mail.ru>
2023-01-15 18:15:29 +0300

    Create TestRunner class

---
+++ tests/test-runner/TestRunner.cpp
@@ -0,0 +1,4 @@
+#include "TestRunner.h"
+
+void TestRunner::runAllTests() {
+}
---
+++ tests/test-runner/TestRunner.h
@@ -0,0 +1,13 @@
+#ifndef TEST_CHECKER_TESTRUNNER_H
+#define TEST_CHECKER_TESTRUNNER_H
+
+
+#include <cassert>
+
+class TestRunner {
+public:
+    static void runAllTests();
+};
+
+
+#endif
