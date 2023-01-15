e2c92677a4cec13cad80f9270ee292575d4a4dd8
Volkov Daniil <volkov.study@mail.ru>
2023-01-15 18:50:37 +0300

    Add method for running TestsStackTests in TestRunner.cpp

--- tests/test-runner/TestRunner.cpp
+++ tests/test-runner/TestRunner.cpp
@@ -1,4 +1,12 @@
 #include "TestRunner.h"
 
+#include "../tests-stack-tests/TestsStackTests.h"
+
+void testTestsStack();
+
 void TestRunner::runAllTests() {
+}
+
+void testTestsStack() {
+    TestsStackTests::testPushingTop();
 }
