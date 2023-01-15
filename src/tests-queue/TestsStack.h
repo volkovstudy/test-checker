641952d08b6207dd6810d02e08b9708135600b14
Volkov Daniil <volkov.study@mail.ru>
2023-01-15 18:29:15 +0300

    Add prototype of TestsStack#pushTop(string student)

--- src/tests-queue/TestsStack.h
+++ src/tests-queue/TestsStack.h
@@ -1,9 +1,13 @@
 #ifndef TEST_CHECKER_TESTSSTACK_H
 #define TEST_CHECKER_TESTSSTACK_H
 
+#include <string>
+
+using namespace std;
 
 class TestsStack {
-
+public:
+    void pushTop(string student);
 };
 
 
