9e3c11c0309d59b121a5b04709b1c18f0184e847
Volkov Daniil <volkov.study@mail.ru>
2023-01-15 18:49:44 +0300

    Create TestsStackTests#testPushingTop()

--- tests/tests-stack-tests/TestsStackTests.cpp
+++ tests/tests-stack-tests/TestsStackTests.cpp
@@ -1,1 +1,11 @@
 #include "TestsStackTests.h"
+
+void TestsStackTests::testPushingTop() {
+    TestsStack testsStack;
+
+    testsStack.pushTop("Bob");
+    testsStack.pushTop("Alex");
+
+    assert(testsStack.getIndexOf("Bob") == 0);
+    assert(testsStack.getIndexOf("Alex") == 1);
+}
--- tests/tests-stack-tests/TestsStackTests.h
+++ tests/tests-stack-tests/TestsStackTests.h
@@ -1,8 +1,12 @@
 #ifndef TEST_CHECKER_TESTSSTACKTESTS_H
 #define TEST_CHECKER_TESTSSTACKTESTS_H
 
+#include <cassert>
+#include "../../src/tests-stack/TestsStack.h"
 
 class TestsStackTests {
+public:
+    static void testPushingTop();
 
 };
 
